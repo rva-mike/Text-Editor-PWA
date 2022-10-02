630be9bf4b931d7ed4fe53b066e0719a458d8964
rva-mike <mike.levy.rva@gmail.com>
2022-10-02 13:42:21 -0400

    workbox plugins for service worker and manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -21,7 +21,33 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         template: "./index.html",
         title: "JATE",
-      })
+      }),
+
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Text-Editor',
+        short_name: 'JATE',
+        description: 'Just another text editor!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
+
+
     ],
 
     module: {
