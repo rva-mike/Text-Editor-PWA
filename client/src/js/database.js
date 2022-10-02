de2ebf2a08097573e992551e519c80bc8764738c
rva-mike <mike.levy.rva@gmail.com>
2022-10-02 13:56:37 -0400

    added logic for db

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,3 +1,4 @@
+
 import { openDB } from 'idb';
 
 const initdb = async () =>
@@ -13,9 +14,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// export const putDb = async (content) => console.error('putDb not implemented');
+// activity 24 PWA
+export const putDb = async (content) =>{
+  console.log('Put request and update the jate db');
+  const jateDB = await openDB('jate', 1);// connect to the database, version 1
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');// create object store
+  // use put method to add in id and value
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log('🚀 - data saved to the jateDB', result);
 
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
-
+// export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () =>{
+  console.log('Get all data from jate db');
+  const jateDB = await openDB('jate', 1);
+  const tx = jateDB.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  // use getALL() method to get all content from db
+  const request = store.getAll();;
+  const result = await request;
+  console.log('🚀 - data saved to the jateDB', result);
+};
 initdb();
